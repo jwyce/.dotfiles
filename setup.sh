22f70847f375e300fad1b60aeb2cd2677c64703c
Jared Wyce <jared.wyce@nelnet.net>
2022-06-03 09:49:01 -0400

    ✨ Update `setup.sh`

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "🍺 Installing homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "🐠 Installing fish"
 brew install fish

d998f5b89dd4d22bc3dc01d173cf1c8ce34130af
Jared Wyce <wycejared@gmail.com>
2022-06-25 09:20:53 -0400

    🔊 Edit log msg

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ fnm install 16
 fnm use 16
 npm i -g yarn pnpm zx typescript-language-server
 
-echo "🔥 Setting up neovim plugins and oh-my-tmux"
+echo "🔌 Setting up vim.plugged and oh-my-tmux"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/gpakosz/.tmux.git

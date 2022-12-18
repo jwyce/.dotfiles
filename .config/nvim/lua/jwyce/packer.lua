368ad2ef03b644f5c9bcb28e6fca3e23d26571c7
Jared Wyce <wycejared@gmail.com>
2022-12-18 14:32:38 -0500

    ⿴ nvim surround

--- .config/nvim/lua/jwyce/packer.lua
+++ .config/nvim/lua/jwyce/packer.lua
@@ -79,5 +79,13 @@ return require('packer').startup(function(use)
             require("tailwindcss-colors").setup()
         end
     }
-
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 end)

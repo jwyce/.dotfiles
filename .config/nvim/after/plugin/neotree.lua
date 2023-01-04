200590a817a5f7d65d065ad0601839323a61d0bc
Jared Wyce <jared.wyce@nelnet.net>
2023-01-04 13:38:42 -0500

    🌳 add neotree

---
+++ .config/nvim/after/plugin/neotree.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<leader>nt", function() vim.cmd("NeoTreeFocusToggle") end)
--- .config/nvim/lua/jwyce/packer.lua
+++ .config/nvim/lua/jwyce/packer.lua
@@ -71,6 +71,18 @@ return require('packer').startup(function(use)
   use("andweeb/presence.nvim")
   use("sbdchd/neoformat")
 
+  vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+
   use {
     'numToStr/Comment.nvim',
     config = function()

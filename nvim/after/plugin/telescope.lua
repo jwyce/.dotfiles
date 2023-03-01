92f12a4aa46569bc01d1f990d2db0c1976083b09
Jared Wyce <wycejared@gmail.com>
2023-02-28 21:29:21 -0500

    🔭 keybind for telescope media

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,8 @@
 local builtin = require('telescope.builtin')
+local media = require("telescope")
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pm', media.extensions.media_files.media_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });

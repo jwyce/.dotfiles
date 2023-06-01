fa18cfe2385f74111c26ba3cf644d0517e430a3e
Jared Wyce <wycejared@gmail.com>
2023-05-31 20:18:50 -0400

    💼 secret work stuff

--- .zshrc
+++ .zshrc
@@ -45,5 +45,7 @@ alias kc='kubectl'
 alias xsc='pbcopy'
 alias clean_symln='rm -- *(-@D)'
 
+source "$DOT/.nelnet"
+
 # Fig post block. Keep at the bottom of this file.
 [[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.post.zsh"
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -9,7 +9,7 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader>vrc', function()
-    require('jwyce.telescope').search_dotfiles({ hidden = true })
+    require('jwyce.telescope').search_dotfiles()
 end)
 vim.keymap.set('n', '<leader>va', function()
     require('jwyce.telescope').anime_selector()

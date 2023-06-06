f617ee26bfd3b291cf301ecc54ca863233a1a6fd
Jared Wyce <wycejared@gmail.com>
2023-06-06 10:58:34 -0400

    🎨 fix enabled formatters for js

--- nvim/after/plugin/format.lua
+++ nvim/after/plugin/format.lua
@@ -1,4 +1,4 @@
 vim.g.neoformat_try_node_exe = 1
-vim.g.neoformat_enabled_javascript = { "prettier", "prettier-eslint", "deno fmt" }
-vim.g.neoformat_enabled_json = { "prettier", "prettier-eslint", "deno fmt" }
+vim.g.neoformat_enabled_javascript = { "prettier", "prettiereslint", "denofmt" }
+vim.g.neoformat_enabled_json = { "prettier", "prettiereslint", "denofmt" }
 vim.keymap.set("n", "<leader>f", function() vim.cmd("Neoformat") end)

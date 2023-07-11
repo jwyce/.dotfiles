5df10c2ef88db5967ac6da0e2c97b7b6f9e53edd
Jared Wyce <wycejared@gmail.com>
2023-07-10 22:38:27 -0400

    🗺️ adding new remaps!

--- nvim/lua/jwyce/remap.lua
+++ nvim/lua/jwyce/remap.lua
@@ -4,6 +4,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Oil)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")

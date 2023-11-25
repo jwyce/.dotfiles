94f9ae55cad26cb4a73590ba6d547510e14404ad
Jared Wyce <wycejared@gmail.com>
2023-11-25 08:47:21 -0500

    🌳 treeshitter update again

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -23,5 +23,6 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 
-require("ts_context_commentstring").setup({})
-require("Comment").setup()
+require("Comment").setup({
+	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+})

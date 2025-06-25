a29a6d6fb7304a5f83a24ea68f73e1b908f47b83
Jared Wyce <wycejared@gmail.com>
2025-06-25 09:33:43 -0400

    🧙 blink cmp menu simplification

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -11,41 +11,6 @@ require("blink.cmp").setup({
 
 	completion = {
 		documentation = { auto_show = true },
-		menu = {
-			draw = {
-				components = {
-					kind_icon = {
-						ellipsis = false,
-						highlight = function(ctx)
-							local hl = "BlinkCmpKind" .. ctx.kind
-								or require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx)
-							if vim.tbl_contains({ "Path" }, ctx.source_name) then
-								local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-								if dev_icon then
-									hl = dev_hl
-								end
-							end
-							return hl
-						end,
-						text = function(ctx)
-							local icon = ctx.kind_icon
-							if vim.tbl_contains({ "Path" }, ctx.source_name) then
-								local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-								if dev_icon then
-									icon = dev_icon
-								end
-							else
-								icon = require("lspkind").symbolic(ctx.kind, {
-									mode = "symbol",
-								})
-							end
-
-							return icon .. ctx.icon_gap
-						end,
-					},
-				},
-			},
-		},
 	},
 
 	sources = {

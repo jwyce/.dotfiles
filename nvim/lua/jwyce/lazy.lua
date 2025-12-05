c5e0f524d6f0d1e663afb53e0211f14ae49c9f2c
Jared Wyce <wycejared@gmail.com>
2025-12-05 08:20:50 -0500

    🖼️ update image.nvim to fix gif freezes

--- nvim/lua/jwyce/lazy.lua
+++ nvim/lua/jwyce/lazy.lua
@@ -78,7 +78,6 @@ local plugins = {
 	{ "MagicDuck/grug-far.nvim", opts = {} },
 	{
 		"3rd/image.nvim",
-		commit = "21909e3eb03bc738cce497f45602bf157b396672", -- remove nvim 0.11 bug fixed https://github.com/3rd/image.nvim/issues/292#issuecomment-2882821123
 		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
 		opts = {
 			processor = "magick_cli",
